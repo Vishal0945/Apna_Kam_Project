c48180980cd2aecbb875ac1387581041ae28c21b
VishalActGlobal <167847749+VishalActGlobal@users.noreply.github.com>
2026-01-06 15:50:52 +0530

    add response model file

---
+++ apnakam/src/main/java/com/apnakam/model/ResponseModel.java
@@ -0,0 +1,14 @@
+package com.apnakam.model;
+
+import lombok.Data;
+
+@Data
+public class ResponseModel {
+	
+	private Object message;
+	private Object data;
+	private Object httpStatus;
+    private String name;
+    
+    //Vishal
+}
